e87e7d7e9ef77cb418e28ae18bebbc248760f791
richzago97 <rich.czago@gmail.com>
2023-06-06 14:29:38 -0300

    feat: criado validações de url

--- core/ConfigController.php
+++ core/ConfigController.php
@@ -5,15 +5,69 @@ namespace Core;
 class ConfigController
 {
     private string $url;
+    private array $urlArray;
+    private string $urlController;
+    private string $urlParameter;
+    private string $urlSlugController;
+    private array $format;
 
     public function __construct()
     {
-        echo "Carregar a pagina <br>";
+        echo "Carregar a pagina<br>";
         if(!empty(filter_input(INPUT_GET, 'url', FILTER_DEFAULT))){
             $this->url = filter_input(INPUT_GET, 'url', FILTER_DEFAULT);
             var_dump($this->url);
+
+            
+            $this->clearUrl();
+            $this->urlArray = explode("/", $this->url);
+            var_dump($this->urlArray);
+
+            if(isset($this->urlArray[0])){
+                var_dump($this->urlArray[0]);
+                $this->urlController = $this->slugController($this->urlArray[0]);
+            }else{
+                $this->urlController = "Home";
+            }
+
         }else{
             echo "Acessa a página inicial <br>";
+            $this->urlController = "Home";
         }
+        
+        echo "Controller: {$this->urlController} <br>";
+    }
+
+    private function clearUrl()
+    {
+        //Eliminar as tags
+        $this->url = strip_tags($this->url);
+
+        //Eliminar os espaços em branco
+        $this->url = trim($this->url);
+
+        //Eliminar a barra no final da URL
+        $this->url = rtrim($this->url, "/");
+
+        //Eliminar caracteres
+        $this->format['a'] = 'ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖØÙÚÛÜüÝÞßàáâãäåæçèéêëìíîïðñòóôõöøùúûýýþÿRr"!@#$%&*()_-+={[}]?;:.,\\\'<>°ºª ';
+        $this->format['b'] = 'aaaaaaaceeeeiiiidnoooooouuuuuybsaaaaaaaceeeeiiiidnoooooouuuyybyRr-------------------------------------------------------------------------------------------------';
+        $this->url = strtr(mb_convert_encoding($this->url, 'ISO-8859-1', 'UTF-8'), mb_convert_encoding($this->format['a'], 'ISO-8859-1', 'UTF-8'), $this->format['b']);
+    }
+
+    private function slugController($slugController)
+    {
+        //Converter para minusculo
+        $this->urlSlugController = strtolower($slugController);
+
+        //Converter o traco para espaco em braco
+        $this->urlSlugController = str_replace("-", " ", $this->urlSlugController);
+
+        //Converter a primeira letra de cada palavra para maiusculo
+        $this->urlSlugController = ucwords($this->urlSlugController);
+        
+        //Retirar espaco em branco
+        $this->urlSlugController = str_replace(" ", "", $this->urlSlugController);
+        return $this->urlSlugController;
     }
 }
