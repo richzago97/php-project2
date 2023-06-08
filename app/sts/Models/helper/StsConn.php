35796349252bd4e7302e4272ae3c1a96b3123358
richzago97 <rich.czago@gmail.com>
2023-06-08 18:00:45 -0300

    feat: criado configuração para conectar ao banco

---
+++ app/sts/Models/helper/StsConn.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Sts\Models\helper;
+
+use PDO;
+use PDOException;
+
+if(!defined("C7E3L8K9E5")){
+    //header("Location: /");
+    die("Erro: Página não encontrada!");
+ }
+
+class StsConn
+{
+    private string $host = HOST;
+    private string $user = USER;
+    private string $pass = PASS;
+    private string $dbname = DBNAME;
+    private int|string $port = PORT;
+    private object $connect;
+
+    public function connectDb(): object
+    {
+        try {
+            $this->connect = new PDO("mysql:host={$this->host};port={$this->port};dbname=" . 
+            $this->dbname, $this->user, $this->pass);
+
+            return $this->connect;
+        } catch (PDOException $err) {
+            die("Erro: Tente novamente. Caso o problema persista, entre em conato 
+            com o administrador " . EMAILADM);
+        }
+    }
+
+
+}
