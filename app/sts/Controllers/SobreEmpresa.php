7bc6fcd8abd620ce4da1bcdf16aeee57d3c447e6
richzago97 <rich.czago@gmail.com>
2023-06-08 17:58:44 -0300

    feat: adicionado doc e proteção

--- app/sts/Controllers/SobreEmpresa.php
+++ app/sts/Controllers/SobreEmpresa.php
@@ -2,10 +2,28 @@
 
 namespace Sts\Controllers;
 
+if(!defined("C7E3L8K9E5")){
+    //header("Location: /");
+    die("Erro: Página não encontrada!");
+ }
+
 class SobreEmpresa
 {
+    private array|string|null $data;
+    
+    /**
+     * @var array $data Recebe os dados que devem ser enviados para VIEW
+    */
+
+    /**
+     * Instanciar a classe responsável em carregar a View
+     * @return void
+     */
+
     public function index()
     {
-        echo "Página sobre empresa<br>";
+        $this->data = [];
+        $loadView = new \Core\ConfigView("sts/Views/sobreEmpresa/sobreEmpresa", $this->data);
+        $loadView->loadView();
     }
 }
