b342465adc1d2ccc6221cc7cb991046097821a9d
richzago97 <rich.czago@gmail.com>
2023-06-08 17:58:18 -0300

    feat: adicionado doc e proteção

--- app/sts/Controllers/Contato.php
+++ app/sts/Controllers/Contato.php
@@ -2,10 +2,28 @@
 
 namespace Sts\Controllers;
 
+if(!defined("C7E3L8K9E5")){
+    //header("Location: /");
+    die("Erro: Página não encontrada!");
+ }
+
 class Contato
 {
+    private array|string|null $data;
+
+    /**
+     * @var array $data Recebe os dados que devem ser enviados para VIEW
+    */
+ 
+    /**
+     * Instanciar a classe responsável em carregar a View
+     * @return void
+     */
+
     public function index()
     {
-        echo "Página de contato<br>";
+        $this->data = "Formulário enviado com sucesso! <br>";
+        $loadView = new \Core\ConfigView("sts/Views/contato/contato", $this->data);
+        $loadView->loadView();
     }
 }
