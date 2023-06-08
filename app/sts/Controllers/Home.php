eccff61493e2cbe6c81951c38533bc612c72ff75
richzago97 <rich.czago@gmail.com>
2023-06-08 17:58:38 -0300

    feat: adicionado doc e proteção

--- app/sts/Controllers/Home.php
+++ app/sts/Controllers/Home.php
@@ -2,10 +2,29 @@
 
 namespace Sts\Controllers;
 
+if(!defined("C7E3L8K9E5")){
+    //header("Location: /");
+    die("Erro: Página não encontrada!");
+ }
+
 class Home
 {
+    /**
+     * @var array $data Recebe os dados que devem ser enviados para VIEW
+    */
+    private array|string|null $data;
+
+    /**
+     * Instanciar a classe responsável em carregar a View
+     * @return void
+     */
+
     public function index()
     {
-        echo "Página Inicial<br>";
+        $home = new \Sts\Models\StsHome();
+        $this->data = $home->index();
+
+        $loadView = new \Core\ConfigView("sts/Views/home/home", $this->data);
+        $loadView->loadView();
     }
 }
