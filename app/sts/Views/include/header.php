f5bca069199557a4532b1d65e22951ff51ceb4ce
richzago97 <rich.czago@gmail.com>
2023-06-08 18:01:49 -0300

    feat: feito a separação do codigo html

---
+++ app/sts/Views/include/header.php
@@ -0,0 +1,16 @@
+<?php
+    if(!defined("C7E3L8K9E5")){
+        //header("Location: /");
+        die("Erro: Página não encontrada!");
+     }
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+    
+<head>
+    <meta charset="UTF-8">
+    <title>Project 2</title>
+</head>
+
+<body>
