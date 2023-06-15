3681202ae7f1bd656b5ffd44f8b3394614c33549
richzago97 <rich.czago@gmail.com>
2023-06-15 19:13:23 -0300

    feat: extraido os dados e configurado a view

--- app/sts/Views/home/home.php
+++ app/sts/Views/home/home.php
@@ -5,6 +5,15 @@ if(!defined("C7E3L8K9E5")){
    die("Erro: Página não encontrada!");
 }
 
-echo "View da pagina home<br> ";
+echo "<h2> Página Inicial </h2>";
 
-var_dump($this->data);
+extract($this->data[0]);
+
+echo "ID: $id <br>";
+echo "Título: $title_top <br>";
+echo "Descrição: $description_top <br>";
+echo "Link: $link_btn_top <br>";
+echo "Texto: $txt_btn_top <br>";
+echo "Imagem: $image <br>";
+
+
